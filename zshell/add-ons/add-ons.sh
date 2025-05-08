e674822ca14e499b87f9fdcec1fb76b0cf078acc
Larry Shields <shieldsl@upmc.edu>
2025-05-08 08:17:51 -0400

    Added Spark Configurations

--- zshell/add-ons/add-ons.sh
+++ zshell/add-ons/add-ons.sh
@@ -1,6 +1,4 @@
 # Oh My Z-Shell Configuration
-
-
 if [[ -d "$HOME/.oh-my-zsh" ]]; then;
 
     export ZSH="$HOME/.oh-my-zsh"
@@ -11,11 +9,12 @@ if [[ -d "$HOME/.oh-my-zsh" ]]; then;
     source $ZSH/oh-my-zsh.sh
 fi
 
-
+# PyEnv Python Version Manager
 if  [[ -d "$HOME/.pyenv" ]]; then;
     eval "$(pyenv init -)"
 fi
 
+# Node Version Manager
 if [[ -d "$HOME/.nvm" ]]; then;
 
     export NVM_DIR="$HOME/.nvm"
@@ -23,8 +22,18 @@ if [[ -d "$HOME/.nvm" ]]; then;
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
 
+# SDK Man
 if [[ -d "$HOME/.sdkman" ]]; then;
 
     export SDKMAN_DIR="$HOME/.sdkman"
     [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
+
+# Spark Configuration
+if [[ ! -z "${SPARK_HOME}" ]]; then;
+    export PYSPARK_PYTHON="python"
+
+    if [[ -d "$HOME/spark_config" ]]; then;        
+        export SPARK_CONF_DIR="$HOME/spark_config"
+    fi
 fi
